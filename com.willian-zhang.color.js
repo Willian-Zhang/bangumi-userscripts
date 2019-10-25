caed1c720986b629d0d59e9e64bbea793c90a826
Willian Zhang <willian@willian-zhang.com>
2019-10-26 02:33:59 +0800

    Update com.willian-zhang.color.js

--- com.willian-zhang.color.js
+++ com.willian-zhang.color.js
@@ -1,26 +1,45 @@
-/* credit: https://github.com/micro-js/hsl-to-rgb/blob/master/lib/index.js */
+/* credit: https://css-tricks.com/converting-color-spaces-in-javascript/ */
 
-function HSL2RGB (h, s, l) {
-    // Achromatic
-    if (s === 0) return [l, l, l]
-    h /= 360
-  
-    let q = l < 0.5 ? l * (1 + s) : l + s - l * s
-    let p = 2 * l - q
-  
-    return [
-      Math.round(HUE2RGB(p, q, h + 1/3) * 255),
-      Math.round(HUE2RGB(p, q, h) * 255),
-      Math.round(HUE2RGB(p, q, h - 1/3) * 255)
-    ]
-}
+function HSL2RGB(h, s, l) {
+    // Must be fractions of 1
+    s /= 100;
+    l /= 100;
 
-function HUE2RGB (p, q, t) {
-    if (t < 0) t += 1
-    if (t > 1) t -= 1
-    if (t < 1/6) return p + (q - p) * 6 * t
-    if (t < 1/2) return q
-    if (t < 2/3) return p + (q - p) * (2/3 - t) * 6
-  
-    return p
+    let c = (1 - Math.abs(2 * l - 1)) * s,
+        x = c * (1 - Math.abs((h / 60) % 2 - 1)),
+        m = l - c / 2,
+        r = 0,
+        g = 0,
+        b = 0;
+
+    if (0 <= h && h < 60) {
+        r = c; g = x; b = 0;
+    } else if (60 <= h && h < 120) {
+        r = x; g = c; b = 0;
+    } else if (120 <= h && h < 180) {
+        r = 0; g = c; b = x;
+    } else if (180 <= h && h < 240) {
+        r = 0; g = x; b = c;
+    } else if (240 <= h && h < 300) {
+        r = x; g = 0; b = c;
+    } else if (300 <= h && h < 360) {
+        r = c; g = 0; b = x;
+    }
+    r = Math.round((r + m) * 255);
+    g = Math.round((g + m) * 255);
+    b = Math.round((b + m) * 255);
+
+    r = r.toString(16);
+    g = g.toString(16);
+    b = b.toString(16);
+
+    if (r.length == 1)
+        r = "0" + r;
+    if (g.length == 1)
+        g = "0" + g;
+    if (b.length == 1)
+        b = "0" + b;
+
+    return "#" + r + g + b;
+
 }
