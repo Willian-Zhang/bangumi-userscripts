6b5d8f424856d5777517ab5287104e7ea065a8d7
Willian Zhang <willian@willian-zhang.com>
2019-10-26 03:27:26 +0800

    Update com.willian-zhang.highlight-eps.js

--- com.willian-zhang.highlight-eps.js
+++ com.willian-zhang.highlight-eps.js
@@ -1,29 +1,33 @@
 // ==UserScript==
 // @name         Highlight ep#
 // @namespace    com.willian-zhang.highlight-eps
-// @version      1.04
+// @version      2.0
 // @description  Highlight Episode Number
 // @author       Willian
 // @match        http*://share.dmhy.org/*
 // @match        http*://bangumi.moe/*
 // @match        http*://share.xfsub.com*/sort-*
 // @require      https://code.jquery.com/jquery-2.1.4.min.js
+// @require      https://raw.githubusercontent.com/Willian-Zhang/bangumi-userscripts/master/com.willian-zhang.color.js
 // @grant        unsafeWindow
 // @run-at       document-end
 // ==/UserScript==
 
 const angular = unsafeWindow.angular;
 
-const colors = [
-    "#FF0097",
-    "#A200FF",
-    "#00ABA9",
-    "#8CBF26",
-    "#E671B8",
-    "#F09609",
-    "#1BA1E2"
+const [S, L] = [50, 75];
+const Hs = [
+    0, +120, +240, 
+    60, 60+120, 60+240, 
+    90, 90+120, 90+240,
+    30, 30+120, 30+240, 
 ];
-const epRegex = /((.+)([\s|\[|【|第]))(\d{1,3}(?:\.\d)?(?:[-|~]\d{1,3})?)(([集|話|话|\s|\]|】])(.*))/;
+const colors = Hs.map(H=> HSL2RGB(H, S, L));
+console.log(`Colors: ${colors.length}`);
+
+const epRegex = /((.+)(([\s|\[|【|第])(?:\d{1,3}[-|~])?(\d{1,3})(?:\.\d)?(?:v\d)?([集|話|话|\s|\]|】]))(.*))/;
+// TODO: named
+// ((?<prefix>.+)(?<epText>([\s|\[|【|第])(?:\d{1,3}[-|~])?(?<ep>\d{1,3})(?:\.\d)?(?:v\d)?([集|話|话|\s|\]|】]))(?<suffix>.*))
 
 const highlightMe = function(){
     let $element = $(this);
@@ -32,13 +36,18 @@ const highlightMe = function(){
     }
     var text = $element.text().trim();
     var found = epRegex.exec(text);
-    if(found !== null){
-        let ep = Number(found[4]) >-1 ? Number(found[4]) : 0;
+    if(found){
+        let prefix = found[2];
+        let epText = found[3];
+        let ep = Number(found[5]);
+        let suffix = found[7];
+
+        ep = Math.max(ep, 0);
         let color = colors[ep % colors.length];
         $element.empty().append([
-            document.createTextNode(found[1]),
-            `<highlight style="background-color: ${color}">${found[4]}</highlight>`,
-            document.createTextNode(found[5])
+            document.createTextNode(prefix),
+            `<highlight style="background-color: ${color}">${epText}</highlight>`,
+            document.createTextNode(suffix)
         ]);
     }else{
         console.log(text);
