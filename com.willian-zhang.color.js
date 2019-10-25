08d5065fe502910dc67b622d4d5ecdd2e0ee2a9a
Willian Zhang <willian@willian-zhang.com>
2019-10-26 02:12:54 +0800

    Update com.willian-zhang.color.js

--- com.willian-zhang.color.js
+++ com.willian-zhang.color.js
@@ -9,8 +9,18 @@ function HSL2RGB (h, s, l) {
     let p = 2 * l - q
   
     return [
-      Math.round(hueToRgb(p, q, h + 1/3) * 255),
-      Math.round(hueToRgb(p, q, h) * 255),
-      Math.round(hueToRgb(p, q, h - 1/3) * 255)
+      Math.round(HUE2RGB(p, q, h + 1/3) * 255),
+      Math.round(HUE2RGB(p, q, h) * 255),
+      Math.round(HUE2RGB(p, q, h - 1/3) * 255)
     ]
+}
+
+function HUE2RGB (p, q, t) {
+    if (t < 0) t += 1
+    if (t > 1) t -= 1
+    if (t < 1/6) return p + (q - p) * 6 * t
+    if (t < 1/2) return q
+    if (t < 2/3) return p + (q - p) * (2/3 - t) * 6
+  
+    return p
 }
