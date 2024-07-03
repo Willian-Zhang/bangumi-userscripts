afd1bee30629e3f22e2991e99a0e0da58a11ed0d
Willian Z <willian@willian.email>
2024-07-03 16:10:55 +0800

    Update com.willian-zhang.highlight-eps.js

--- com.willian-zhang.highlight-eps.js
+++ com.willian-zhang.highlight-eps.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name         Highlight ep#
 // @namespace    com.willian-zhang.highlight-eps
-// @version      2.4
+// @version      2.4.2
 // @description  Highlight Episode Number
 // @author       Willian
+// @match        http*://dmhy.org/
 // @match        http*://dmhy.org/*
 // @match        http*://share.dmhy.org/*
 // @match        http*://bangumi.moe/*
@@ -12,23 +13,22 @@
 // @require      https://code.jquery.com/jquery-2.1.4.min.js
 // @require      https://raw.githubusercontent.com/Willian-Zhang/bangumi-userscripts/master/com.willian-zhang.color.js
 // @grant        unsafeWindow
-// @run-at       document-end
 // ==/UserScript==
 
 // const angular = unsafeWindow.angular;
 
 const [S, L] = [50, 75];
 const Hs = [
-    0, +120, +240, 
-    60, 60+120, 60+240, 
+    0, +120, +240,
+    60, 60+120, 60+240,
     90, 90+120, 90+240,
-    30, 30+120, 30+240, 
+    30, 30+120, 30+240,
 ];
 const colors = Hs.map(H=> HSL2RGB(H, S, L));
 console.log(`Colors: ${colors.length}`);
 
 // https://regex101.com/r/4ovR28/
-const epRegex = /((.+)((\s|\[|【|第|EP)(?:\d{1,3}[-|~])?(\d{1,3})(?:\.\d)?(?:v\d)?(?:TV)?(集|話|话|\s|\]|】))(.*))/;
+const epRegex = /((.+)((\s|\[|【|第|EP)(?:\d{1,4}[-|~])?(\d{1,4})(?:\.\d)?(?:v\d)?(?:TV)?(集|話|话|\s|\]|】))(.*))/;
 // TODO: named
 // ((?<prefix>.+)(?<epText>([\s|\[|【|第])(?:\d{1,3}[-|~])?(?<ep>\d{1,3})(?:\.\d)?(?:v\d)?([集|話|话|\s|\]|】]))(?<suffix>.*))
 
@@ -57,6 +57,7 @@ const highlightMe = function(){
     }
 };
 if(/bangumi.moe/.test(document.location.host)){
+    console.log('Highlighting Bangumi');
     $(document).on("mouseenter",'[torrent-list]',function(e){
         let titleElements = $(this).find(".md-item-raised-title");
 
@@ -65,6 +66,7 @@ if(/bangumi.moe/.test(document.location.host)){
         titleElements.on("mouseenter",highlightMe);
     });
 }else if(/dmhy.org/.test(document.location.host)){
+    console.log('Highlighting DMHY');
     $(document).ready(function(){
         let table = $(".table  table > tbody");
         let titles = table.find('tr > td.title > a');
@@ -76,6 +78,7 @@ if(/bangumi.moe/.test(document.location.host)){
     /share.xfsub.com/.test(document.location.host) ||
     /share.xfapi.top/.test(document.location.host)
     ){
+    console.log('Highlighting XFSub');
     $(document).ready(function(){
         let table = $("#listTable > tbody");
         let titles = table.find('tr > td:nth-child(2) > a:last-child');
@@ -83,5 +86,7 @@ if(/bangumi.moe/.test(document.location.host)){
         titles.off("mouseenter");
         titles.on("mouseenter",highlightMe);
     });
+} else{
+    console.log('NO MATCH for Highlighting')
 }
 
